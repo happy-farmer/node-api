6b28961a903ad208d27649f144172dbd9fcde809
dmi3y <lapshukov@gmail.com>
2016-05-23 23:17:55 -0700

    server instead of hardcoded url for supertest, tweaks in the way how tests invoked, separate env variables

--- tasks/test.sh
+++ tasks/test.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-tape ./tests/**/*.js
+APP_ENV_VARS="PORT=6789 DBURL=mongodb://localhost:27017/testapi"
+env $APP_ENV_VARS tape ./tests/**/*.js
--- tests/farmsSpec.js
+++ tests/farmsSpec.js
@@ -1,25 +1,29 @@
 var test = require('tape')
 var request = require('supertest')
-// var app = require(process.cwd() + '/server')
+var app = require(process.cwd() + '/server')
 var validate = require(process.cwd() + '/validator')
 
-test('GET /farms', (t) => {
-  t.plan(3)
+app.then((server) => {
+  test('GET /farms', (t) => {
+    t.plan(3)
 
-  request('http://localhost:3004')
-    .get('/farms')
-    .expect('Content-Type', /json/)
-    .expect(200)
-    .end((err, res) => {
-      var body = res.body
-      console.log(body)
-      var msg = validate.response(body, '/farms/get')
+    request(server)
+      .get('/farms')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        var body = res.body
+        var msg = validate.response(body, '/farms/get')
 
-      console.log(msg)
+        t.error(err, 'no error expected')
+        t.ok(Array.isArray(body), 'has to be collection')
+        t.ok(msg.valid, 'has to be valid against schema')
 
-      t.error(err, 'no error expected')
-      t.ok(Array.isArray(body), 'body has to be array')
-      t.ok(msg.valid, 'has to be valid against schema')
-      t.end()
-    })
+        t.end()
+      })
+  })
+
+  test.onFinish(() => {
+    server.close()
+  })
 })

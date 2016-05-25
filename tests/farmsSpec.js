34c0462935396d7f8a1fc6a7680630492cd4dbb8
dmi3y <lapshukov@gmail.com>
2016-05-24 22:44:51 -0700

    more tests for farms endpoint

--- tests/farmsSpec.js
+++ tests/farmsSpec.js
@@ -14,11 +14,58 @@ app.then((server) => {
       .expect(200)
       .end((err, res) => {
         var body = res.body
-        var msg = validate.response(body, '/farms/get')
+        t.error(err, 'no errors expected')
 
-        t.error(err, 'no error expected')
-        t.ok(Array.isArray(body), 'has to be collection')
+        var msg = validate.response(body, '/farms/get')
         t.ok(msg.valid, 'has to be valid against schema')
+
+        t.ok(Array.isArray(body), 'has to be collection')
+
+        t.end()
+      })
+  })
+
+  test('POST /farms (valid request)', (t) => {
+    t.plan(2)
+
+    var item = {
+      'name': 'name',
+      'location': {
+        'street-address': 'SOME str 123',
+        'city': 'San Francisco',
+        'state': 'CA'
+      }
+    }
+
+    request(server)
+      .post('/farms')
+      .send(item)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .end((err, res) => {
+        var body = res.body
+        var id = body.id
+        t.error(err, 'no errors expected')
+
+        var msg = validate.response(body, '/farms/post')
+        t.ok(msg.valid, 'has to be valid against schema')
+
+        test(`GET /farms/${id}`, (t) => {
+          t.plan(2)
+
+          request(server)
+            .get(`/farms/${id}`)
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+              var body = res.body
+              t.error(err, 'no errors expected')
+
+              var msg = validate.response(body, '/farms/:id/get')
+              t.ok(msg.valid, 'has to be valid against schema')
+            })
+        })
 
         t.end()
       })
@@ -33,8 +80,8 @@ app.then((server) => {
       .expect(400)
       .end((err, res) => {
         var body = res.body
+        t.error(err, 'no errors expected')
 
-        t.error(err, 'no error expected')
         t.equal(body.status, 400, 'should have status')
         t.ok(_.has(body, 'statusMessage'), 'should have status message')
         t.ok(_.has(body, 'technicalMessage'), 'should have technical message')
@@ -51,14 +98,51 @@ app.then((server) => {
       .expect(404)
       .end((err, res) => {
         var body = res.body
+        t.error(err, 'no errors expected')
 
-        t.error(err, 'no error expected')
         t.equal(body.status, 404, 'should have status')
         t.ok(_.has(body, 'statusMessage'), 'should have status message')
         t.end()
       })
   })
 
+  test('GET /farms/does_not_exist', (t) => {
+    t.plan(4)
+
+    request(server)
+      .get('/farms/does_not_exist')
+      .expect('Content-Type', /json/)
+      .expect(501)
+      .end((err, res) => {
+        var body = res.body
+        t.error(err, 'no errors expected')
+
+        t.equal(body.status, 501, 'should have status')
+        t.ok(_.has(body, 'statusMessage'), 'should have status message')
+        t.ok(_.has(body, 'technicalMessage'), 'should have technical message')
+        t.end()
+      })
+  })
+
+  test('POST /farms (invalid request)', (t) => {
+    t.plan(4)
+
+    request(server)
+      .post('/farms')
+      .send({})
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .end((err, res) => {
+        var body = res.body
+        t.error(err, 'no errors expected')
+
+        t.equal(body.status, 400, 'should have status')
+        t.ok(_.has(body, 'statusMessage'), 'should have status message')
+        t.ok(_.has(body, 'technicalMessage'), 'should have technical message')
+        t.end()
+      })
+  })
   test.onFinish(() => {
     server.close()
   })

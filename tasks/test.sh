77f69573570670310790b95f0e7950137936fed5
dmi3y <lapshukov@gmail.com>
2016-05-26 00:33:18 -0700

    changing testing database name

--- tasks/test.sh
+++ tasks/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-APP_ENV_VARS="PORT=6789 DBURL=mongodb://localhost:27017/testapi"
+APP_ENV_VARS="PORT=6789 DBURL=mongodb://localhost:27017/test_api_test"
 env $APP_ENV_VARS tape ./tests/**/*Spec.js

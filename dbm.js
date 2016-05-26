f10c6fd594948cc38f542b268b666321d49a0128
dmi3y <lapshukov@gmail.com>
2016-05-25 22:52:25 -0700

    enhanced object methods literals

--- dbm.js
+++ dbm.js
@@ -9,25 +9,29 @@ var debug = require('debug')('api:dbm')
 var state = {}
 
 module.exports = {
-  connect: (url) => new Promise(function (resolve, reject) {
-    if (state.db) {
-      resolve(state.db)
-    } else {
-      debug('connecting to database...')
-      MongoClient.connect(url, (err, db) => {
-        if (err) {
-          reject(err)
-        } else {
-          debug('connected to database')
-          state.db = db
-          resolve(db)
-        }
-      })
-    }
-  }),
-  close: () => {
+  connect (url) {
+    return new Promise(function (resolve, reject) {
+      if (state.db) {
+        resolve(state.db)
+      } else {
+        debug('connecting to database...')
+        MongoClient.connect(url, (err, db) => {
+          if (err) {
+            reject(err)
+          } else {
+            debug('connected to database')
+            state.db = db
+            resolve(db)
+          }
+        })
+      }
+    })
+  },
+  close () {
     debug('closing database connection...')
     state.db.close()
   },
-  get: (col) => state.db.collection(col)
+  get (col) {
+    return state.db.collection(col)
+  }
 }
--- error-handler.js
+++ error-handler.js
@@ -1,7 +1,7 @@
 var debug = require('debug')('api:error-handler')
 
 module.exports = {
-  validationRequest: (err, req, res, next) => {
+  validationRequest (err, req, res, next) {
     err.technicalMessage = err.error.message
     debug('validation request error occured')
     if (err.valid !== null) {
@@ -20,26 +20,26 @@ module.exports = {
     debug('preset error occured')
     next(err, req, res, next)
   },
-  validationResponse: (err, req, res, next) => {
+  validationResponse (err, req, res, next) {
     err.statusMessage = 'Not valid response'
     err.technicalMessage = err.error.message
     err.status = 501
     debug('validation response error occured')
     next(err, req, res, next)
   },
-  dbmWrite: (err, req, res, next) => {
+  dbmWrite (err, req, res, next) {
     err.statusMessage = 'Database write error'
     err.technicalMessage = err.errmsg
     debug('database write error occured')
     next(err, req, res, next)
   },
-  dbmRead: (err, req, res, next) => {
+  dbmRead (err, req, res, next) {
     err.statusMessage = 'Database read error'
     err.technicalMessage = err.errmsg
     debug('database read error occured')
     next(err, req, res, next)
   },
-  notFound: (err, req, res, next) => {
+  notFound (err, req, res, next) {
     err.statusMessage = 'Not found'
     err.technicalMessage = err.message || err.errmsg
     err.status = 404
@@ -47,7 +47,7 @@ module.exports = {
     next(err, req, res, next)
   },
   // 501 is new 404
-  catchAll404: (req, res, next) => {
+  catchAll404 (req, res, next) {
     var statusMessage = 'Not implemented'
     var status = 501
     var technicalMessage = 'Catch 404 all stage'
@@ -61,7 +61,7 @@ module.exports = {
     }
     next(err, req, res, next)
   },
-  catchAll: (err, req, res, next) => {
+  catchAll (err, req, res, next) {
     var statusMessage = err.statusMessage || 'Catch all stage'
     var status = err.status || 500
     var technicalMessage = err.technicalMessage || err.message
